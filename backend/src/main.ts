4f7e67f38b6ffbdd03bb90867a770a7a11861559
AyKrimino <ayoub.krimi14@gmail.com>
2025-10-05 22:24:14 +0100

    feat(backend): add origins to enableCors & updated backend/package.json scripts for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 4000);
+  app.enableCors({
+    origin: [
+      'https://aykrimino.github.io/resume-generator',
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+  await app.listen(port);
+  console.log(`Listening on ${port}`);
 }
 bootstrap();
