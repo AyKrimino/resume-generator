4bcf4d97ade530c4a649ff4808bb1e964e8d7058
AyKrimino <ayoub.krimi14@gmail.com>
2025-10-06 08:17:58 +0100

    feat: configure production baseURL and add Puppeteer postinstall script

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    "https://resume-generator-00w9.onrender.com",
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
