fa41bbfe9b912e22f40ef5528d22e845cc9ad4e0
AyKrimino <ayoub.krimi14@gmail.com>
2025-10-06 08:43:00 +0100

    fix(backend): fix CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,13 +3,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const frontOrigins = [
+    'https://aykrimino.github.io',
+    'https://aykrimino.github.io/resume-generator',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+  ];
+
+  const origin = process.env.FRONTEND_ORIGIN
+    ? process.env.FRONTEND_ORIGIN.split(',')
+    : frontOrigins;
+
   app.enableCors({
-    origin: [
-      'https://aykrimino.github.io/resume-generator',
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: origin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With, Accept',
     credentials: true,
   });
   const port = process.env.PORT ? Number(process.env.PORT) : 4000;
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -4,10 +4,10 @@ const apiClient = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_BASE_URL ||
     "https://resume-generator-00w9.onrender.com",
-  timeout: 1000,
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: false,
 });
 
 export default apiClient;
